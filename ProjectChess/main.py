435c7222e66412eda60509596e3e4201e5e779ed
GabrielGenkov <gabriel.genkov2002@gmail.com>
2020-03-20 16:33:56 +0200

    Added error messages and hashing passwords

--- ProjectChess/main.py
+++ ProjectChess/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, request, render_template, session
-from datetime import timedelta
+import hashlib
+#from datetime import timedelta
 
 
 from user import User
@@ -10,39 +11,56 @@ app.secret_key = '''Qp5NWkGtNvAk0Ti1JAiwvuFve6KiZtAvur86xVd9k7LVTXYz4qGoer7n9DXT
 j4l26AS3q5cRF6IovlyTwC4N0dSZAKs4uobWNhNuN2NNWANjZDesyKMWItSkGMRUc8XG9j7k8yQnftUB
 y5USFesLJ4bgnLl3YVdSq8MNaEmjWOcxjMU8j6N05c9qHyINGerKtiDmnY7U'''
 
-app.permanent_session_lifetime = timedelta(minutes = 60)
+#app.permanent_session_lifetime = timedelta(minutes = 60)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 	if request.method == 'GET':
-		return render_template('signin.html')
+		error = None
+		if "r_err" in session:
+			error = session["r_err"]
+		return render_template('signin.html', error = error)
 	elif request.method == 'POST':
-		session.permanent = True
 		values = (
 			None,
 			request.form['username'],
 			request.form['mail'],
-			request.form['password']
+			hashlib.sha1((request.form['password'] + "itscoronatime").encode('utf-8')).hexdigest()
 		)
 		user = User(*values).create()
 		if user:
 			session["user"] = user.mail
-		return redirect('/')
+			if "r_err" in session:
+				session.pop("r_err", None)
+			return redirect('/')
+		session["r_err"] = "This accaunt already exists!!"
+		return redirect('/register')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if request.method == 'GET':
-		return render_template('login.html')
+		error = None
+		if "l_err" in session:
+			error = session["l_err"]
+		return render_template('login.html', error = error)
 	elif request.method == 'POST':
 		mail = request.form['mail']
-		password = request.form['password']
+		password = hashlib.sha1((request.form['password'] + "itscoronatime").encode('utf-8')).hexdigest()
 		user = User.load(mail, password)
 		if user:
 			session["user"] = user.mail
-	return redirect('/')
+			if "l_err" in session:
+				session.pop("l_err", None)
+			return redirect('/')
+		session["l_err"] = "Acccaunt with that email and password doesn't exists!!"
+		return redirect('/login')
 
 @app.route('/')
 def main():
+	if "r_err" in session:
+		session.pop("r_err", None)
+	if "l_err" in session:
+		session.pop("l_err", None)
 	user = None
 	if "user" in session:
 		user = User.load1(session["user"])
