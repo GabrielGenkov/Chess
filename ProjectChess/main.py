4ecc50c6152d52b4320db0d6d94d9d4f0ced7919
GabrielGenkov <gabriel.genkov2002@gmail.com>
2020-05-15 17:55:47 +0300

    ready for adding modes

--- ProjectChess/main.py
+++ ProjectChess/main.py
@@ -21,6 +21,11 @@ socketio = SocketIO(app)
 users = dict()
 positions = dict()
 histories = dict()
+modes = dict()
+modes['default'] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+modes['horde'] = "rnbqkbnr/pppppppp/8/1PP2PP1/PPPPPPPP/PPPPPPPP/PPPPPPPP/PPPKPPPP w kq - 0 1"
+
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -41,8 +46,8 @@ def register():
 		)
 		user = User(*values).create()
 		if user:
-			user = User.load1(user.mail)
-			session["user"] = user.mail
+			user = User.load2(user.id)
+			session["user"] = user.id
 			return redirect('/')
 		session["r_err"] = "This accaunt already exists!!"
 		return redirect('/register')
@@ -59,7 +64,7 @@ def login():
 		password = hashlib.sha1((request.form['password'] + "itscoronatime").encode('utf-8')).hexdigest()
 		user = User.load(mail, password)
 		if user:
-			session["user"] = user.mail
+			session["user"] = user.id
 			return redirect('/')
 		session["l_err"] = "Acccaunt with that email and password doesn't exists!!"
 		return redirect('/login')
@@ -72,7 +77,7 @@ def main():
 		session.pop("l_err", None)
 	user = None
 	if "user" in session:
-		user = User.load1(session["user"])
+		user = User.load2(session["user"])
 	return render_template('index.html',user = user)
 
 @app.route('/logout')
@@ -80,55 +85,56 @@ def logout():
 	session.pop("user", None)
 	return redirect('/')
 
-@app.route("/default/<int:id>")
-def chatroom(id):
+@app.route("/<mode>/<int:id>")
+def chatroom(mode,id):
 	global users
 	global positions
 	global histories
+	global modes
 	second_player = False
 	if not "user" in session:
 		return redirect('/')
 	if not User.load2(id):
 		return redirect('/')
-	if User.load1(session["user"]).mail != User.load2(id).mail and str(id) not in users:
+	if session["user"] != id and mode + str(id) not in users:
 		return redirect('/')
-	if User.load1(session["user"]).mail == User.load2(id).mail and str(id) not in users:
-		users[str(id)] = list()
-		positions[str(id)] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-		histories[str(id)] = ""
-	if User.load1(session["user"]).mail != User.load2(id).mail and str(id) in users:
-		if(len(users[str(id)]) == 1):
+	if session["user"] == id and mode + str(id) not in users:
+		users[mode + str(id)] = list()
+		positions[mode + str(id)] = modes[mode]
+		histories[mode + str(id)] = ""
+	if session["user"] != id and mode + str(id) in users:
+		if(len(users[mode + str(id)]) == 1):
 			second_player = True
-		if(len(users[str(id)]) >= 2 and users[str(id)][1] == session["user"]):
+		if(len(users[mode + str(id)]) >= 2 and users[mode + str(id)][1] == session["user"]):
 			second_player = True
-	return render_template("room.html", user=User.load1(session["user"]), host = User.load2(id), position = positions[str(id)], history = histories[str(id)] ,second_player = second_player)
+	return render_template("room.html", user=User.load2(session["user"]), host = User.load2(id), position = positions[mode + str(id)], history = histories[mode + str(id)] ,second_player = second_player, mode = mode)
 	
 @socketio.on("send message")
 def message(data):
     channel = data['channel']
     message = data['message']
-    emit('broadcast message', message, channel)
+    user = data['user']
+    emit('broadcast message', user + " : " + message, channel)
 
 @socketio.on('join')
 def on_join(data):
 	global users
-	username = data['username']
 	channel = data['channel']
+	username = User.load2(data['id']).username
 	if str(channel) in users:
-		if data['mail'] not in users[str(channel)]:
-			users[str(channel)].append(data['mail'])
-			emit('broadcast users', users[str(channel)], channel)
+		if data['id'] not in users[str(channel)]:
+			users[str(channel)].append(data['id'])
 		print(users[str(channel)])
 	join_room(channel)
 	emit('broadcast message', username + ' has connected!', channel)
 
 @socketio.on('leave')
 def on_leave(data):
-	username = data['username']
 	channel = data['channel']
+	username = User.load2(data['id']).username
 	if str(channel) in users:
-		if data['mail'] in users[str(channel)]:
-			users[str(channel)].remove(data['mail'])
+		if data['id'] in users[str(channel)]:
+			users[str(channel)].remove(data['id'])
 	leave_room(channel)
 	emit('broadcast message', username + ' has left the room.', channel)
 
